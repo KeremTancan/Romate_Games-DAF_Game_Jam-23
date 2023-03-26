29f017e000ad0c31088e9fa1a4b2ae3fc02ae282
KeremTancan <tancankerem149@gmail.com>
2023-03-26 16:39:22 +0300

    Game Over

--- Assets/Scripts/UnlemScript.cs
+++ Assets/Scripts/UnlemScript.cs
@@ -6,19 +6,21 @@ using UnityEngine.SceneManagement;
 public class UnlemScript : MonoBehaviour
 {
     public int ButonSayisi = 6;
-    
+
     public Inventory inventory;
     public GameObject Button;
 
+
     private void Update()
+    {
+        ButtonCheck();
+
+    }
+    public void ButtonCheck()
     {
         if (ButonSayisi == 0)
         {
             Button.SetActive(false);
-           
         }
-
-        
     }
-    
 }
